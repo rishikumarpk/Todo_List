bcb9e751ac9782e1ecb7566d21b4e0fba8e178c0
rishikumarpk <pkrishikumar2468@gmail.com>
2026-03-02 23:31:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const db = new pg.Pool({
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.PORT || 5432,
+  port: process.env.DB_PORT || 5432,
   ssl: process.env.DB_HOST && process.env.DB_HOST !== "localhost" 
        ? { rejectUnauthorized: false } 
        : false
